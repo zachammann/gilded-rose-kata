d0ce8c4a62dad6829125dda625ebd83f47ff6c13
Hannah Henderson <hannah@circleci.com>
2018-12-21 07:27:03 -0800

    Refactor slightly in preparation

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -1,6 +1,9 @@
 (ns gilded-rose.core-spec
 (:require [clojure.test :refer :all]
-          [gilded-rose.core :refer [update-quality item]]))
+          [gilded-rose.core :as core]))
+
+(def items [(core/item "foo" 0 0)])
 
 (deftest gilded-rose-test
-  (is (= "fixme" (:name (first (update-quality [(item "foo" 0 0)]))))))
+  (is (= "fixme" (:name (first (core/update-quality items)))))
+  (is (= 1 1)))
