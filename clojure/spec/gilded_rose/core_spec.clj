edce8365acdd42a167e1dae1d232517d966c0d10
Hannah Henderson <hannah@circleci.com>
2019-01-02 19:13:22 -0800

    Refactor kata based on three intern interviews
    This is a lot to throw at someone that has never used clojure
    before. If you have a one-hour interview slot and are comfortable
    pairing with someone junior, this is one heck of a way to see
    how someone responds to discomfort and an unknown codebase and
    language.
    
    I strongly recommend articulating repeatedly that you expect
    this to be difficult.
    
    I would avoid going through the README entirely, that's a waste
    of time during a short interview.

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -2,8 +2,79 @@
 (:require [clojure.test :refer :all]
           [gilded-rose.core :as core]))
 
-(def items [(core/item "foo" 0 0)])
+(comment
+  This is a sample codebase for testing. The Gilded Rose is an imaginary grocery
+  store in a fantasy world. It sells some weird products. Like any grocery store,
+  the products lose quality (value) the longer they sit on the shelf (with a few
+  exceptions we are unlikely to get to).
+
+  Let's work through these TODOs:
+    1. Write some tests asserting what `core/update-quality` should return right now.
+    2. Update the function so that aged brie doubles in value every 30 days.
+    3. Update the function to cap quality at 50.
+
+  It is super entirely possible we won't make it past the first TODO. This is a TON
+  of new information all at once.
+
+  I am your resource, ask me any questions, there are seriously no dumb ones. The
+  more you talk through your logic out loud, the easier it is for me to see how you
+  approach problems.
+
+  I can run tests any time, and am happy to give you examples of how functions work
+  in the REPL. If something doesn't make sense, please ask me and we'll figure it
+  out together.
+  )
+
+(def items [(core/item "foo" 0 0)
+            (core/item "Aged Brie" 5 27)
+            (core/item "bar" 0 9)])
 
 (deftest gilded-rose-test
+  (is (= 1 1))
+
   (is (= "fixme" (:name (first (core/update-quality items)))))
-  (is (= 1 1)))
+
+  ; (is (= "fixme" (:quality (first (core/update-quality items)))))
+
+  )
+
+
+(comment
+  ; Some handy examples to use in your repl
+
+  (+ 1 1)           ; (1 + 1)
+  (+ 1 1 1 1)       ; (1 + 1 + 1 + 1)
+  (> 9 0)
+
+  (first [5 4 3])
+  (second [5 4 3])
+  (last [5 4 3])
+
+  (:a {:a "howdy" :b "what's up"})
+
+  (merge {:a 1 :b 2 :c 3} {:d 4})
+  (merge {:a 1 :b 2 :c 3} {:c 900 :d 4})
+
+  (map inc [1 2 3 60 15])
+  (map (fn [x] (str x "!")) ["a" "b" 50 4 "hi"])
+
+  (defn cond-example [x]
+    (cond
+      (= 1 0)
+      (println "false")
+
+      (= 1 5)
+      (println "also false")
+
+      (= 1 x)
+      (println "what do you know, x = 1, it's true!")
+
+      :else
+      (println "no true values here")))
+
+  (defn if-example [x]
+    (if (= x 1)
+      (println "true: x = 1")
+      (println "false: x != 1")))
+
+  )
--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -2,13 +2,18 @@
 
 (defn update-quality* [item]
   (cond
-    (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
+    (and (< (:sell-in item) 0)
+         (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
     (merge item {:quality 0})
     
-    (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-    (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
+    (or (= (:name item) "Aged Brie")
+        (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
+    (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert")
+             (>= (:sell-in item) 5)
+             (< (:sell-in item) 10))
       (merge item {:quality (inc (inc (:quality item)))})
-      (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
+      (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert")
+               (>= (:sell-in item) 0) (< (:sell-in item) 5))
         (merge item {:quality (inc (inc (inc (:quality item))))})
         (if (< (:quality item) 50)
           (merge item {:quality (inc (:quality item))})
@@ -17,23 +22,27 @@
     (< (:sell-in item) 0)
     (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
       (merge item {:quality 0})
-      (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+      (if (or (= "+5 Dexterity Vest" (:name item))
+              (= "Elixir of the Mongoose" (:name item)))
         (merge item {:quality (- (:quality item) 2)})
         item))
 
-    (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+    (or (= "+5 Dexterity Vest" (:name item))
+        (= "Elixir of the Mongoose" (:name item)))
     (merge item {:quality (dec (:quality item))})
 
     :else item))
 
+(defn update-sell-in [items]
+  (map
+    (fn [item]
+      (if (= "Sulfuras, Hand of Ragnaros" (:name item))
+        item
+        (merge item {:sell-in (dec (:sell-in item))})))
+    items))
+
 (defn update-quality [items]
-  (map update-quality*
-       (map
-         (fn [item]
-           (if (= "Sulfuras, Hand of Ragnaros" (:name item))
-             item
-             (merge item {:sell-in (dec (:sell-in item))})))
-         items)))
+  (map update-quality* (update-sell-in items)))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
